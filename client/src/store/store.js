e618ee73d2e23bb67ff5ad7eb89a7953a92a60a4
т <>
2026-03-12 18:52:18 +0300

    titles Slice for Events titles

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,10 @@
 ﻿import { configureStore } from '@reduxjs/toolkit';
 import eventsReducer from './eventsSlice.js';
+import titlesReducer from './titlesSlice.js';
 
 export const store = configureStore({
   reducer: {
     events: eventsReducer,
+    titles: titlesReducer,
   },
 });
